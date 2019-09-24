67a53f0ba00c5867cab34fba79f489846f4d7a20
Roel Voordendag <rvoordendag@gmail.com>
2019-09-24 22:48:14 +0200

    added config to gitignore

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome at the swiping api'
+    return 'Welcome at the swiping api.'
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
