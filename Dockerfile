f623ad0c24547fd7884f3a9667988eff4c0c1bdb
Roel Voordendag <roelvoordendag@Roels-MacBook-Pro.local>
2019-10-06 16:24:07 +0200

    psuhed docker file and requirements

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Simple Dockerfile can be expanded
+FROM python:3.7
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+ENTRYPOINT ["python"]
+
+CMD ["start.py"]
--- start.py
+++ start.py
@@ -9,7 +9,8 @@ app.register_blueprint(swipe_route, url_prefix='/swipe')
 
 @app.route('/')
 def hello_world():
-    return 'Welcome at the swiping api.'
+    return 'Welcome at the swiping api. Made by the enjuneers!'
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
+    # app.run(host='0.0.0.0', debug=True, use_reloader=True)
