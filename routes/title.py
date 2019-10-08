3d14fe1923e6480fa150095b4e842fe693190e5d
Roel Voordendag <roelvoordendag@Roels-MacBook-Pro.local>
2019-10-08 21:54:48 +0200

    new user with no data in binary still get a title

--- routes/title.py
+++ routes/title.py
@@ -19,24 +19,26 @@ def returnTitles():
     else:  # check for post with userid and everything
         if 'id' in request.json and request.json['id'] != '':
             id = request.json['id']  # id from the post with userid
-
-            titlesSeen = list(binarySet.aggregate([  # the list is to transform the cursor to a list
-                {'$match': {'userId': id}},
-                {'$group':
-                 {
-                     '_id': 0,
-                     'titleKeys': {
-                         '$push': '$primary_key'
+            if binarySet.find({'userId': id}).count() > 0:
+                titlesSeen = list(binarySet.aggregate([  # the list is to transform the cursor to a list
+                    {'$match': {'userId': id}},
+                    {'$group':
+                     {
+                         '_id': 0,
+                         'titleKeys': {
+                             '$push': '$primary_key'
+                         }
                      }
-                 }
-                 }
-            ]))[0]['titleKeys']  # get only list of key
+                     }
+                ]))[0]['titleKeys']  # get only list of key
 
-            title = collectionTitles.aggregate([
-                {'$match': {'primary_key': {'$nin': titlesSeen}}},
-                {'$sample': {'size': 1}},
-                {'$project': {'title': 1, 'description': 1, 'url': 1, 'primary_key': 1, 'timestamp': 1, '_id': 0}}
-            ])
+                title = collectionTitles.aggregate([
+                    {'$match': {'primary_key': {'$nin': titlesSeen}}},
+                    {'$sample': {'size': 1}},
+                    {'$project': {'title': 1, 'description': 1, 'url': 1, 'primary_key': 1, 'timestamp': 1, '_id': 0}}
+                ])
+            else:
+                title = collectionTitles.aggregate([{'$sample': {'size': 1}}, {'$project': {'title': 1, 'description': 1, 'url': 1, 'primary_key': 1, 'timestamp': 1, '_id': 0}}])
 
             return dumps(title), 200
         else:
