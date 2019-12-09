a0e5579d0e14c0140ce41904e26bb96c4cb5f697
Roel <rvoordendag@gmail.com>
2019-12-09 16:15:58 +0100

    added post to add more news paper sources

--- routes/adminPrompt/getNewsSources.py
+++ routes/adminPrompt/getNewsSources.py
@@ -7,10 +7,19 @@ news_source_route = Blueprint('news_source_route', __name__)
 # connect to db
 db = client.swiper
 
-
+# TODO: ADD AUTHENTICATION
 @news_source_route.route('/newsSources', methods=['get', 'post'])
 def newsSources():
     # Return all articles
     if request.method == 'GET':
         return(dumps(db.newsProviders.find({})))
-    # TODO ADD A POST TO ADD
+    if request.method == 'POST':
+        if request.json['source'] and len(request.json['source']) > 0:  # Validation
+            print(request.json['source'])
+            newsSourceObject = {"name": request.json['source'], "value": request.json['source']}
+            # insert to DB
+            db.newsProviders.insert(newsSourceObject)
+        else:
+            return dumps({'success': False, 'message': 'Json name is not good/Value is empty'})
+
+        return dumps({'success': True, 'message': 'created'})
