b8612b203f1f59f6fbadc8f85d66e923278fa55d
Roel <rvoordendag@gmail.com>
2019-12-20 14:33:39 +0100

    Login/auth is fixed on all places only need to add users in the mongoDB

--- routes/adminPrompt/login.py
+++ routes/adminPrompt/login.py
@@ -1,10 +1,10 @@
-import datetime
 import argon2
 from hashlib import sha256
 from argon2 import PasswordHasher, verify_password
 from flask import Blueprint, request
 from ..connection import client
 from bson.json_util import dumps
+from datetime import datetime, timedelta
 
 ph = PasswordHasher()
 login_route = Blueprint('login_route', __name__)
@@ -25,15 +25,17 @@ def loginRoute():
         except argon2.exceptions.VerifyMismatchError:
             return dumps({'success': False, 'message': 'Credits are not correct!'}), 300
 
-        time = datetime.datetime.utcnow()
+        time = datetime.utcnow()
         apiKey = request.json['username'] + format(time.hour) + format(time.minute) + format(time.second) + format(time.microsecond)
 
         # Set collection
         collection = db.apiKey
+        # Drop old indexes otherwise mongo will keep using the old ones
+        collection.drop_indexes()
 
-        collection.create_index('inserted at: ' + format(datetime.datetime.utcnow()), expireAfterSeconds=60)
-        collection.insert_one({'apiKey': apiKey, 'user': request.json['username'], "inserted": datetime.datetime.utcnow()})
+        collection.create_index("Index of this exprire", expireAfterSeconds=1200)
+        collection.insert_one({'apiKey': apiKey, 'user': request.json['username'], "inserted": datetime.utcnow()})
 
-        return dumps({'success': True, 'message': 'You are logged in', 'apiKey': 'apikey'}), 200
+        return dumps({'success': True, 'message': 'You are logged in', 'apiKey': apiKey}), 200
     else:
         return dumps({'success': False, 'message': 'Username not valid or username not filled in'}), 404
